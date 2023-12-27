6a332e6cf2a84bc9b91ba35b08acf5a7097f59cb
Alessandro Pecorilla <APecorillaLearning@Yahoo.com>
2023-12-27 10:07:18 +0100

    Updated package.json file with desc and created first basic express server

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Task Manager App')
+// Requiring the 'express' and 'morgan' modules
+const express = require('express')
+const morgan = require('morgan')
+// Creating an Express application
+const app = express()
+// Use morgan
+app.use(morgan('dev'))
+
+// Routes
+app.get('/api/v1/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+
+// Setting the server to listen on port 6575
+const port = 6575
+app.listen(port, () => {
+    console.log(`Server is listening on port : ${port}`)
+})
